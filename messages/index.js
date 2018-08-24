e528d48053b8ba6116e054b969c66efbc453f8c5
Marcio Toshio <marcio@tosh.io>
2018-08-23 22:22:47 -0300

    usd to brl rate

--- messages/index.js
+++ messages/index.js
@@ -3,39 +3,50 @@ var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
 
+const rate = require('./usd_brl_rate');
+
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    openIdMetadata: process.env['BotOpenIdMetadata']
+  appId: process.env['MicrosoftAppId'],
+  appPassword: process.env['MicrosoftAppPassword'],
+  openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
 /*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* Bot Storage: This is a great spot to register the private state storage for your bot.
 * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
 * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
 * ---------------------------------------------------------------------------------------- */
 
-var tableName = 'botdata';
-var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
-
 var bot = new builder.UniversalBot(connector);
 bot.localePath(path.join(__dirname, './locale'));
-bot.set('storage', tableStorage);
+
+if (!useEmulator) {
+  var tableName = 'botdata';
+  var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+  var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+  bot.set('storage', tableStorage);
+}
 
 bot.dialog('/', function (session) {
+  var msg = session.message.text;
+  if(msg.includes('show rate')) {
+    rate.get_rate(function(value){
+      session.send(value);
+    });
+  } else {
     session.send('You said ' + session.message.text);
+  }
 });
 
 if (useEmulator) {
-    var restify = require('restify');
-    var server = restify.createServer();
-    server.listen(3978, function() {
-        console.log('test bot endpont at http://localhost:3978/api/messages');
-    });
-    server.post('/api/messages', connector.listen());    
+  var restify = require('restify');
+  var server = restify.createServer();
+  server.listen(3978, function() {
+      console.log('test bot endpont at http://localhost:3978/api/messages');
+  });
+  server.post('/api/messages', connector.listen());
 } else {
-    module.exports = connector.listen();
+  module.exports = connector.listen();
 }
---
+++ messages/usd_brl_rate.js
@@ -0,0 +1,23 @@
+const https = require('https');
+
+module.exports = {
+   get_rate: function(callback) {
+      https.get('https://free.currencyconverterapi.com/api/v5/convert?q=USD_BRL&compact=y', (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          var value = JSON.parse(data).USD_BRL.val;
+          callback('Current rate: R$ ' + value + '\n\nPayoneer: ~R$: ' + value*0.98);
+        });
+
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+   }
+}
